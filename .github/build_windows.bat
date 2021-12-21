75ee30b1e5978617d8598cf6505888dbafe825d6
Niyati Maheshwari <niyatim23@gmail.com>
2021-12-20 16:46:54 -0800

    fix builds

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
 mkdir build
 cd build
 cmd.exe /c cmake -G "NMake Makefiles" ..
