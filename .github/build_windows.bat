950343c0c1b7d24fdcd9cc3a996878c56cab7454
Niyati Maheshwari <niyatim23@gmail.com>
2022-02-02 13:57:55 -0800

    update batch file

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64 8.1
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64 10.0.10240.0 -vcvars_ver=14.0
 mkdir build
 cd build
 cmd.exe /c cmake -G "NMake Makefiles" ..
