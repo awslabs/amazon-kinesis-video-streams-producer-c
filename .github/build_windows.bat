f66e7ac2de7984ed12ec52dcba2a09b0308c957e
Niyati Maheshwari <niyatim23@gmail.com>
2022-02-04 11:25:07 -0800

    try x86

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,7 +1,6 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
 mkdir build
 cd build
 cmd.exe /c cmake -G "NMake Makefiles" ..
 cmake -G "NMake Makefiles" -DBUILD_TEST=TRUE ..
 nmake
-"D:\a\amazon-kinesis-video-streams-producer-c\amazon-kinesis-video-streams-producer-c\build\tst\producer_test.exe" --gtest_break_on_failure
