df64788a6cbef72a7e95fa68b1cc989ba21f3c52
Niyati Maheshwari <niyatim23@gmail.com>
2022-02-07 16:31:08 -0800

    fix windows build on travis

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
 cmd.exe /c cmake -G "NMake Makefiles" ..
