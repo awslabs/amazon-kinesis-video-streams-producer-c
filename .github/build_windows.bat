2fb90e0ff4475df2e1d3d9d3270e6ba5bf637638
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2024-03-14 14:07:26 -0700

    Build local curl for windows

--- .github/build_windows.bat
+++ .github/build_windows.bat
@@ -1,4 +1,4 @@
-REM  NOTE: May need to replace "Enterprise" with "Community" int eh below command
+REM  NOTE: May need to replace "Enterprise" with "Community" int the below command
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 mkdir build
 cd build
